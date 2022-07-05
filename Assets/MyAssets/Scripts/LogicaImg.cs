825131cd94796118052a2b676eee0f6fcc1e7aac
osukadesu <64621867+osukadesu@users.noreply.github.com>
2022-07-05 08:57:36 -0500

    Completo

--- Assets/MyAssets/Scripts/LogicaImg.cs
+++ Assets/MyAssets/Scripts/LogicaImg.cs
@@ -19,7 +19,7 @@ public class LogicaImg : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-     public void Correcto2()
+    public void Correcto2()
     {
         SceneManager.LoadScene(4);
     }
@@ -29,7 +29,7 @@ public class LogicaImg : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
-        public void Error2()
+    public void Error2()
     {
         SceneManager.LoadScene(5);
     }
@@ -42,5 +42,60 @@ public class LogicaImg : MonoBehaviour
     public void JugarAgain2()
     {
         SceneManager.LoadScene(3);
+    }
+
+    public void ErrorSuma()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void JugarAgainSuma()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Winsuma()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void JugarResta()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+     public void ErroResta()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void JugarAgainResta()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Winresta()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void JugarError()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void ErroError()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+    public void JugarAgainError()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void WinError()
+    {
+        SceneManager.LoadScene(13);
     }
 }
